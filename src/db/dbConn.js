23957ce87201a9530777da99fa5ca55953a79517
sakibhusainzaidi2000@gmail.com <sakibhusainzaidi2000@gmail.com>
2023-04-16 02:35:15 +0530

    Updated

--- src/db/dbConn.js
+++ src/db/dbConn.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 // Connect to the MongoDB database
-mongoose.connect('mongodb://127.0.0.1:27017/e-commerce', {
+mongoose.connect(
+  'mongodb://127.0.0.1:27017/e-commerce',
+  
+   {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => console.log('Connected to MongoDB'))
